254aa6a68169918b57e90f0a14353e43ac777a8b
femoscopy <ogunyemifemioladimeji@gmail.com>
2022-10-20 17:17:32 -0700

    fixed

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * more_numbers - print numbers 10 time
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int count, i;
+
+	count = 0;
+	for (count = 0; count < 10; count++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i > 9)
+			{
+				_putchar((i / 10) + '0');
+			}
+			_putchar((i % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
