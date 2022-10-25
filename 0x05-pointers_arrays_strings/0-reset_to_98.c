6a1836734826047c1769cc3ecc04efcae842dce6
femoscopy <ogunyemifemioladimeji@gmail.com>
2022-10-25 02:58:34 -0700

    fixed

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * reset_to_98 - reset using pointer
- * @n: the number tobe pointed
- * Return: void
- * /
+ *  * reset_to_98 - reset using pointer
+ *   * @n: the number tobe pointed
+ *    * Return: void
+ *     */
 void reset_to_98(int *n)
 {
 	*n = 98;
