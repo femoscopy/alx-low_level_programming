7f1b09b78cc8ab3d69558008cba57153fc1e4362
femoscopy <ogunyemifemioladimeji@gmail.com>
2022-10-25 14:33:06 -0700

    fixed

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,12 +10,12 @@ void puts_half(char *str)
 int len, n, i;
 
 for (len = 0; str[len] != '\0'; len++)
-	{
-		}
-	n = (len - 1) / 2;
-  for (i = n + 1; str[i] != '\0'; i++)
-	{
-	_putchar(str[i]);
-		}
-	_putchar('\n');
+{
+}
+n = (len - 1) / 2;
+for (i = n + 1; str[i] != '\0'; i++)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
 }
