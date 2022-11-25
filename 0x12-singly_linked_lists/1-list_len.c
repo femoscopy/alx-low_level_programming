90eb54ef818e78cbcc037587561759288f4b8245
femoscopy <ogunyemifemioladimeji@gmail.com>
2022-11-25 14:58:38 -0800

    alx_

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,19 +1,23 @@
 #include "lists.h"
 /**
- * list_len - returns then number of elements in a list.
-* @h: singly linked list.
-* Return: number of elements in the list.
+ * print_list - prints all the elements of a list_t list.
+ * @h: singly linked list.
+ * Return: number of elements in the list.
  */
 
-size_t list_len(const list_t *h)
+size_t print_list(const list_t *h)
 {
 	size_t nelem;
 
-	notlem = 0 ;
+	nelem = 0;
 	while (h != NULL)
 	{
+		if (h->str == NULL)
+			printf("[%d] %s\n", 0, "(nil)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
-		don't ++;
+		nelem++;
 	}
 	return (nelem);
 }
